3807f270014d6bbea7cd0e993fbcb90417052c3e
k_nuc <keto1472@gmail.com>
2026-08-12 02:08:44 +0000

    add ip address between ASs

--- scratch/small_internet.cc
+++ scratch/small_internet.cc
@@ -13,6 +13,7 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <set>
 
 using namespace ns3;
 
@@ -20,6 +21,17 @@ struct NeighborInfo {
     uint32_t asn;
     std::string relationship;
 };
+
+struct InterfacesInfo{
+    uint32_t interfaceId;
+    Ipv4Address address;
+    Ipv4Mask mask;
+};
+
+// 初期設定のアドレスとマスク
+ns3::Ipv4Address baseAddress("10.0.0.0");
+ns3::Ipv4Mask netMask("255.255.255.252");
+
 
 std::map<uint32_t, std::vector<NeighborInfo>> ReadTopologyFromFile(const std::string& filename) {
     std::map<uint32_t, std::vector<NeighborInfo>> topology;
@@ -98,6 +110,11 @@ int main (int argc, char *argv[])
   p2p.SetChannelAttribute ("Delay", StringValue ("2ms"));
 
   std::set<std::pair<uint32_t, uint32_t>> createdLinks;
+  
+  std::map<uint32_t, std::map<uint32_t, InterfacesInfo>> asinterfaces;
+  ns3::Ipv4AddressHelper ipv4;
+  Ipv4StaticRoutingHelper staticRoutingHelper;
+
 
   uint32_t link_num = 0;
   uint32_t as_num = 0;
@@ -121,14 +138,48 @@ int main (int argc, char *argv[])
             NodeContainer link(node1, node2);
             NetDeviceContainer devices = p2p.Install(link);
 
-            createdLinks.insert({asn, neighbor_asn});
+            createdLinks.insert({small_asn, large_asn});
             link_num++;
+            std::cout << "Number of links: " << link_num << std::endl;
+
+            ipv4.SetBase(baseAddress, netMask);
+            // assignメソッドを使わず、自分で実装 (assignはIPの重複確認の処理が入っているが、時間がかかるので不採用)
+            // Ipv4InterfaceContainer interfaces = ipv4.Assign(devices);
+
+            Ptr<Ipv4> ipv4Node1 = node1->GetObject<Ipv4>();
+            Ptr<Ipv4> ipv4Node2 = node2->GetObject<Ipv4>();
+
+            int32_t if1 = ipv4Node1->GetInterfaceForDevice(devices.Get(0));
+            if (if1 == -1) {
+                if1 = ipv4Node1->AddInterface(devices.Get(0));
+            }
+
+            int32_t if2 = ipv4Node2->GetInterfaceForDevice(devices.Get(1));
+            if (if2 == -1) {
+                if2 = ipv4Node2->AddInterface(devices.Get(1));
+            }
+
+            Ipv4Address address1(baseAddress.Get() + 1);
+            Ipv4Address address2(baseAddress.Get() + 2);
+
+            ipv4Node1->AddAddress(if1, Ipv4InterfaceAddress(address1, netMask));
+            ipv4Node2->AddAddress(if2, Ipv4InterfaceAddress(address2, netMask));
+
+            ipv4Node1->SetMetric(if1, 1);
+            ipv4Node2->SetMetric(if2, 1);
+
+            ipv4Node1->SetUp(if1);
+            ipv4Node2->SetUp(if2);
+
+            asinterfaces[asn][neighbor_asn] = {if1, address1, netMask};
+            asinterfaces[neighbor_asn][asn] = {if2, address2, netMask};
+
+            baseAddress = ns3::Ipv4Address(baseAddress.Get() + 4);
         }
         as_num++;
+        std::cout << "Number of AS nodes: " << as_num << std::endl;
     }
 
-    std::cout << "Number of AS nodes: " << as_num << std::endl;
-    std::cout << "Number of links: " << link_num << std::endl;
 
     Simulator::Stop(Seconds(simTime));
     Simulator::Run();
